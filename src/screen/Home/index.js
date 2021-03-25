827090246daa7e092f9e7f3ea947c8e0a593c509
Mansi <61904568+monilyf@users.noreply.github.com>
2021-03-25 12:37:40 +0530

    Add files via upload

--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -11,7 +11,7 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// import styles from './style'
+import styles from './style'
 const DATA = [
   {
     id: '1',
@@ -87,50 +87,52 @@ export class Home extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:'#fff',
-    // marginTop: 10,
-  },
-  item: {
-    // backgroundColor:'#fff',
-    marginTop: 20,
-    backgroundColor: '#e0f4f4',
-    padding: 20,
-    marginVertical: 8,
-    borderRadius: 7,
-    borderColor: '#edffec',
-    marginHorizontal: 16,
-    // shadowColor: '#000',
-    // shadowOffset: {
-    //   width: 0,
-    //   height: 2,
-    // },
-    // shadowOpacity: 0.25,
-    // shadowRadius: 3.84,
+// // const styles = StyleSheet.create({
+// //   container: {
+// //     flex: 1,
+// //     backgroundColor:'#fff',
+// //     // marginTop: 10,
+// //   },
+// //   item: {
+// //     // backgroundColor:'#fff',
+// //     marginTop: 20,
+// //     backgroundColor: '#e0f4f4',
+// //     padding: 20,
+// //     marginVertical: 8,
+// //     borderRadius: 7,
+// //     borderColor: '#edffec',
+// //     marginHorizontal: 16,
+// //     // shadowColor: '#000',
+// //     // shadowOffset: {
+// //     //   width: 0,
+// //     //   height: 2,
+// //     // },
+// //     // shadowOpacity: 0.25,
+// //     // shadowRadius: 3.84,
 
-    // elevation: 5,
-  },
-  primaryText:{
-    fontSize:16,
-    marginTop:28,
-    // marginLeft:-10
-  },
-  title: {
-    fontSize: 20,
-    marginLeft: 30,
-    fontWeight:'bold'
-    // marginTop: 20,
-  },
-  detailList: {
-    flexDirection: 'row',
-  },
-  imgStyle: {
-    height: 50,
-    width: 50,
-  },
-  rightIcon: {marginTop: 25, position: 'absolute', right: 20},
-});
+// //     // elevation: 5,
+// //   },
+// //   primaryText:{
+// //     fontSize:16,
+// //     marginTop:28,
+// //     color:'#68abab'
+// //     // marginLeft:-10
+// //   },
+// //   title: {
+// //     fontSize: 20,
+// //     marginLeft: 30,
+// //     fontWeight:'bold',
+// //     color:'#68abab',
+// //     // marginTop: 20,
+// //   },
+// //   detailList: {
+// //     flexDirection: 'row',
+// //   },
+// //   imgStyle: {
+// //     height: 50,
+// //     width: 50,
+// //   },
+// //   rightIcon: {marginTop: 25, position: 'absolute', right: 20},
+// });
 
 export default Home;
